1878b0fa2b8d8f527bd48e8838424aebcc477580
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-05-11 20:08:10 +1000

    .

--- install/nvidia-drivers.sh
+++ install/nvidia-drivers.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # install version 570.133.20
 # 950 MB of additional disk space will be used.
-apt-get install -y --no-install-recommends nvidia-open-570
+apt-get install -y --no-install-recommends nvidia-open-575
 
 # TODO: install nvidia persistence daemon?
 # https://docs.nvidia.com/deploy/driver-persistence/index.html#persistence-daemon
