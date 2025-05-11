83687ff4c0c3535da24f5a18499b0060ddbf0766
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-05-11 20:33:11 +1000

    Revert "use nvidia-open-575 because cuda-runtime-12-2 uses cuda-drivers-575"
    This reverts commit 5fcf531a04a961ca68c0e1d724cfe93e1e2a46d1.
    
    because not released for the data center yet see https://github.com/NVIDIA/open-gpu-kernel-modules/releases

--- install/nvidia-drivers.sh
+++ install/nvidia-drivers.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-# install version 575.51.03
-# 1033 MB of additional disk space will be used.
-apt-get install -y --no-install-recommends nvidia-open-575
+# install version 570.133.20
+# 950 MB of additional disk space will be used.
+apt-get install -y --no-install-recommends nvidia-open-570
 
 # TODO: install nvidia persistence daemon?
 # https://docs.nvidia.com/deploy/driver-persistence/index.html#persistence-daemon
