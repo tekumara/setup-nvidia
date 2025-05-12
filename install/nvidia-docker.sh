7978c2e2ae5e990fa50784bfdb5cddf5168ae2b6
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-05-12 20:36:33 +1000

    make idempotent

--- install/nvidia-docker.sh
+++ install/nvidia-docker.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 # install the Nvidia Container Toolkit to allow Docker to use GPUs
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
       && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
             sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
             tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
