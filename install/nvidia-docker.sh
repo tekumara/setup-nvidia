1e9c782324d72d3341c4df5068ab930344758ba9
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-06-08 14:32:38 +1000

    refactor: output yellow message

--- install/nvidia-docker.sh
+++ install/nvidia-docker.sh
@@ -5,11 +5,14 @@ set -euo pipefail
 # see https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
 
 # install the Nvidia Container Toolkit to allow Docker to use GPUs
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+distribution=$(
+    . /etc/os-release
+    echo $ID$VERSION_ID
+)
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list |
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 apt-get update
 
 # we don't install nvidia-docker2 because it includes containerd, runc and docker.io (349MB)
@@ -20,7 +23,7 @@ apt-get install -y --no-install-recommends nvidia-container-toolkit
 nvidia-ctk runtime configure --runtime=docker
 
 if [[ $(pgrep docker) ]]; then
-    RED='\033[0;31m'
+    YELLOW='\033[0;33m'
     NC='\033[0m' # No Color
-    echo -e "${RED}Restart the docker daemon for the nvidia container toolkit to take effect, eg: systemctl restart docker${NC}"
+    echo -e "${YELLOW}Restart the docker daemon for the nvidia container toolkit to take effect, eg: systemctl restart docker${NC}"
 fi
