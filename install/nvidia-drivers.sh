4b2090133c2fa4dc7a48e95256eb59bdb31f3e36
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-06-08 14:34:55 +1000

    fix(driver): unmet dependencies
    resolves #1

--- install/nvidia-drivers.sh
+++ install/nvidia-drivers.sh
@@ -2,9 +2,12 @@
 
 set -euo pipefail
 
-# install version 570.133.20
-# 950 MB of additional disk space will be used.
-apt-get install -y --no-install-recommends nvidia-open-570
+# install version 570.148.08
+# 1083 MB of additional disk space will be used.
+# Pin nvidia-persistenced because the latest version depends on libnvidia-cfg1
+# which conflicts with nvidia-open-570 -> libnvidia-compute-570 -> libnvidia-cfg1-570
+# Also pin nvidia-open-570 at the same time
+apt-get install -y --no-install-recommends nvidia-persistenced=570.148.08-1ubuntu1 nvidia-open-570=570.148.08-1ubuntu1
 
 # TODO: install nvidia persistence daemon?
 # https://docs.nvidia.com/deploy/driver-persistence/index.html#persistence-daemon
